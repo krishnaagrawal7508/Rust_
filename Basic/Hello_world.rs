ca80c82e35d2910162ac32d954907a4133723d89
krishna agrawal <agrawalkrishna522@gmail.com>
2024-03-23 16:53:02 +0530

    hello world using cargo and rs

---
+++ Basic/Hello_world.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("Hello world");
+}
+
+// 1. Create basic filename.rs file with code
+// 2. run the program using (rustc filename.rs) -> (.\filename.exe) OR use code runner
+// 3. three files will be present now filename.rs, filename.pdb, filename.exe
+// 4. Now, run in terminal (cargo new filename1)
+// 5. New folder will be created with filename1 whcih will have src folder and Cargo.toml
+// 6. Change directory to the filename1
+// 7. Run in terminal, (cargon build) this will create a target folder and a Cargo.lock
+// 8. To execute the code, use (Cargo run)
+// 9. Target folder is not required in the github repository, use .gitignore with target
