99d1cec602d71e860e9cb5bc8a6751c0b6fda5bd
krishna agrawal <agrawalkrishna522@gmail.com>
2024-08-08 18:53:23 +0530

    folder name should not start with number

--- 1_hello_world/src/main.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    println!("Hello, world!");
-}
-
-// 1. run (cargo init)
-// 2. run the program using (cargo run)
-// 3. run (cargon build) this will make main.rs => to executable format
-// 4. Target folder is not required in the github repository, use .gitignore with target
