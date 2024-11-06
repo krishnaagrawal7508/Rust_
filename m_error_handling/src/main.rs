c8496a3bee141f6d5819ff1bfb1758ada713ad30
krishna agrawal <agrawalkrishna522@gmail.com>
2024-11-06 07:17:23 +0530

    -

--- m_error_handling/src/main.rs
+++ m_error_handling/src/main.rs
@@ -1,9 +1,20 @@
-// generic enum, type can be anything
-enum Result<T, E> {
-    Ok(T),
-    Err(E),
-}
+// generic enum, type can be anything, already exist in rust
+// enum Result<T, E> {
+//     Ok(T),
+//     Err(E),
+// }
+
+use std::fs::File;
 
 fn main() {
-    println!("Hello, world!");
+    let res = fs::read_to_string("example.txt");
+
+    match res {
+        Ok(content) => { 
+            println!("File content: {}", content);
+        }
+        Err(err) => {
+            println!("Error: {}", err);
+        }
+    }
 }
